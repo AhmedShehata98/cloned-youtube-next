72ca24d8702828b17d4d77e11b6553e9cd23b760
ahmedshehata98 <userstore98@gmail.com>
2023-01-29 20:47:49 +0200

    finished search result page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    RAPID_API_KEY: process.env.RAPID_API_KEY,
+    NEXT_PUBLIC_RAPID_API_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
     RAPID_API_KEY_Backup: process.env.RAPID_API_KEY_Backup,
   },
 };
