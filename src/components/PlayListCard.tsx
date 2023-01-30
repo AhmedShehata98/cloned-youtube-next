9c7ce2c2f631ad42670ae1d6c461c843db2d28ae
ahmedshehata98 <userstore98@gmail.com>
2023-01-30 17:49:45 +0200

    fixed responsive in video card component

--- src/components/PlayListCard.tsx
+++ src/components/PlayListCard.tsx
@@ -8,13 +8,7 @@ interface IPlayListCardProps {
 
 function PlayListCard({ playListData }: IPlayListCardProps) {
   return (
-    <Link
-      href={{
-        pathname: "/playlist",
-        query: {
-          listId: playListData.id.playlistId,
-        },
-      }}
+    <div
       key={playListData.snippet.channelId}
       className="relative yt-card shadow-sm border border-gray-300"
     >
@@ -22,27 +16,44 @@ function PlayListCard({ playListData }: IPlayListCardProps) {
         className="relative w-full aspect-video"
         title={playListData.snippet.title}
       >
-        <span className="absolute top-0 left-0 w-1/3 h-full flex flex-col items-center justify-center bg-zinc-900 bg-opacity-80 text-white p-2">
+        <span className="absolute top-0 left-0 w-2/5 h-full flex flex-col items-center justify-center bg-zinc-900 bg-opacity-90 text-white p-2">
           <i className="fi fi-rr-list"></i>
           playList
         </span>
-        <figure className="w-full aspect-video">
+        <Link
+          href={{
+            pathname: "/playlist",
+            query: {
+              listId: playListData.id.playlistId,
+            },
+          }}
+          className="w-full aspect-video"
+        >
           <img
             src={playListData.snippet.thumbnails.medium.url}
             alt="playlist-thumb-img"
             className="w-full h-full object-cover aspect-video"
           />
-        </figure>
+        </Link>
       </div>
       <div className="w-full flex-1 flex flex-col p-2 bg-gray-100 border-t-4 border-red-400">
-        <b className="inline-block truncate max-w-full">
-          {playListData.snippet.title}
-        </b>
+        <Link
+          href={{
+            pathname: "/playlist",
+            query: {
+              listId: playListData.id.playlistId,
+            },
+          }}
+        >
+          <bdi className="inline-block truncate max-w-full">
+            {playListData.snippet.title}
+          </bdi>
+        </Link>
         <small className="font-medium opacity-70">
           {playListData.snippet.channelTitle}
         </small>
       </div>
-    </Link>
+    </div>
   );
 }
 
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -14,41 +14,57 @@ function VideoCard({ videoData }: IVideoCardProps) {
   };
 
   return (
-    <Link
-      href={{
-        query: {
-          vidId: videoData.id.videoId,
-        },
-        pathname: "/watch",
-      }}
-      key={videoData?.snippet.title}
-      className="yt-card"
-      title={videoData?.snippet.title}
-    >
-      <figure className="w-full h-fit aspect-video flex items-center justify-center rounded-sm">
+    <div key={videoData?.snippet.title} className="yt-card">
+      <Link
+        href={{
+          query: {
+            vidId: videoData.id.videoId,
+          },
+          pathname: "/watch",
+        }}
+        title={`Go to video ${videoData.snippet.title}`}
+        className="w-full h-fit aspect-video flex items-center justify-center rounded-sm"
+      >
         <img
           src={videoData?.snippet?.thumbnails.medium.url}
           alt={videoData?.snippet?.title}
           className="w-full aspect-video object-cover object-center rounded-sm "
         />
-      </figure>
-      <div className="flex justify-start items-start rounded-sm px-1 py-2 bg-gray-100 gap-1">
+      </Link>
+      <div className="flex justify-start items-start rounded-sm px-1 pt-3 pb-1 bg-gray-100 gap-1">
         <span className="w-[18%] aspect-square bg-red-300 rounded-full flex justify-center items-center">
           <i className="fi fi-rr-desktop-wallpaper leading-3 text-slate-900"></i>
         </span>
         <div className="w-4/5 flex flex-col">
-          <b className="inline-block mb-2 text-sm leading-4 h-8 overflow-hidden">
-            {videoData?.snippet?.title}
-          </b>
-          <p className="text-slate-600 capitalize font-medium">
-            {videoData?.snippet?.channelTitle}
-          </p>
+          <Link
+            href={{
+              query: {
+                vidId: videoData.id.videoId,
+              },
+              pathname: "/watch",
+            }}
+          >
+            <bdi className="inline-block font-semibold text-sm leading-4 h-8 overflow-hidden mb-2">
+              {videoData?.snippet?.title}
+            </bdi>
+          </Link>
+          <Link
+            href={{
+              pathname: "/channels",
+              query: { channelId: videoData.snippet.channelId },
+            }}
+            title={"Go To This Channel"}
+          >
+            <bdi className="text-slate-600 capitalize font-medium text-sm">
+              {videoData?.snippet?.channelTitle}
+            </bdi>
+          </Link>
           <small className="text-gray-500">
             {videoData?.snippet?.publishTime}
           </small>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
