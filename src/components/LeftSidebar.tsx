ada0a0d536a1ef2dfcf19feaf8197998c500f938
ahmedshehata98 <userstore98@gmail.com>
2023-02-06 20:49:29 +0200

    added sideLink component

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 import { categoryBar } from "@/utils/contants";
 import { useRouter } from "next/router";
+import SidebarLink from "./SidebarLink";
 
 interface ILeftSidebarProps {
   show: boolean;
@@ -9,7 +10,7 @@ interface ILeftSidebarProps {
 function LeftSidebar({ show }: ILeftSidebarProps) {
   const sidebarRef = useRef<HTMLElement | null>(null);
 
-  const { query } = useRouter();
+  const { query, pathname } = useRouter();
   // const notPages = pathname.includes("/watch");
 
   useEffect(() => {
@@ -34,55 +35,37 @@ function LeftSidebar({ show }: ILeftSidebarProps) {
   return (
     <aside ref={sidebarRef} className={`sidebar-menu`}>
       <div className="side-category-bar">
-        <ul className="h-fit flex flex-col gap-2 pl-2 mt-2 mb-6 ml-3 md:ml-0">
-          <Link
-            href="/"
-            className="flex gap-4 hover:text-red-500 dark:hover:text-red-400 mb-1"
-          >
-            <span className="grid place-content-center text-xl dark:text-white">
-              <i className="fi fi-rr-home leading-3"></i>
-            </span>
-            <p className="text-base capitalize text-stone-500">home</p>
-          </Link>
-          {/* <Link
-            href={{
-              pathname: "/recent",
-            }}
-            className="flex gap-4 hover:text-red-500 mb-1 dark:hover:text-red-400"
-          >
-            <span className="grid place-content-center text-xl text-white">
-              <i className="fi fi-rr-time-forward leading-3"></i>
-            </span>
-            <p className="text-base capitalize text-stone-500">recent videos</p>
-          </Link> */}
+        <ul className="h-fit flex flex-col pb-4 md:ml-0 gap-2">
+          <SidebarLink
+            href={{ pathname: "/" }}
+            icon="fi-rr-home"
+            label="home"
+            key={"home"}
+            isActive={pathname === "/"}
+          />
+          <SidebarLink
+            href={{ pathname: "/recent" }}
+            icon="fi-rr-time-forward"
+            label="recent"
+            key={"recent"}
+            isActive={pathname === "/recent"}
+          />
         </ul>
-        <ul className="h-fit flex flex-col pb-4 ml-3 md:ml-0">
+        <ul className="h-fit flex flex-col pb-4 ml-3 md:ml-0 gap-2">
           <h5 className="font-semibold capitalize text-lg mb-3 ">explore</h5>
           {categoryBar.map((category, idx) => {
             const isActive = query.category === category.link;
-
             return (
-              <Link
+              <SidebarLink
+                label={category.label}
+                icon={category.icon}
                 key={idx}
+                isActive={isActive}
                 href={{
                   pathname: "/explore",
-                  query: {
-                    category: category.link,
-                  },
+                  query: { category: category.link },
                 }}
-                className={`${
-                  isActive
-                    ? "sidebar-link sidebar-link-active as"
-                    : "sidebar-link "
-                }`}
-              >
-                <span className="grid place-content-center text-lg dark:text-white">
-                  <i className={`fi ${category?.icon} leading-3`}></i>
-                </span>
-                <p className="text-base capitalize opacity-75 ">
-                  {category.label}
-                </p>
-              </Link>
+              />
             );
           })}
         </ul>
